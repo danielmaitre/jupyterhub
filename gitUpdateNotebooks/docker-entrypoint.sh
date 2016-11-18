2e73c37b426544d154cc667ff9b0cebe9079c951
Daniel <daniel.maitre@durham.ac.uk>
2016-11-18 13:49:43 +0000

    added notebookUpdate

---
+++ gitUpdateNotebooks/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+cd /home/jovyan/work/originals
+git pull 
+for n in *.ipynb; do
+    if test -f ../$n; then
+	echo $n already there
+    else
+	cp $n ../$n
+	chown $NB_USER ../$n
+    fi
+done
+
+
